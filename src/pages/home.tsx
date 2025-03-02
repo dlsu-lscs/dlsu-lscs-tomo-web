17961855457b6a69b4f519edb8377ddffe84fcc6
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-03-03 00:39:56 +0800

    fix: change size

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,56 +9,54 @@ export const Home = () => {
   return (
     <>
       <AnimatePresence mode="wait">
-        <div className="min-h-screen bg-[#FEF7E4] text-[#333330]">
-          <div className="flex justify-center items-center min-h-screen min-w-screen">
-            <div className="bg-[#FEC44E] w-screen h-screen md:w-[50vw] md:h-[100vw]">
-              <motion.div
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -10 }}
-                transition={{ duration: 0.25 }}
-              >
-                <div className="flex flex-col items-center justify-center px-8 py-16 font-bold space-y-4">
-                  <div className="text-[#FFFF] text-5xl md:text-6xl">
-                    <h1 className="text-center">tomo coffee x lscs</h1>
-                  </div>
-                  <div>
-                    <h1>Scan to Verify LSCS Membership</h1>
-                  </div>
+        <div className="min-h-screen bg-[#FEC44E] text-[#333330]">
+          <div className="flex justify-center">
+            <motion.div
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -10 }}
+              transition={{ duration: 0.25 }}
+            >
+              <div className="flex flex-col items-center justify-center px-8 py-16 font-bold space-y-4">
+                <div className="text-[#FFFF] text-5xl md:text-6xl">
+                  <h1 className="text-center">tomo coffee x lscs</h1>
                 </div>
-                {!('currentUser' in cookies) ? (
-                  <>
-                    <motion.div
-                      initial={{ opacity: 0, y: 10 }}
-                      animate={
-                        cookies.currentUser
-                          ? { opacity: 1, y: 0 }
-                          : { opacity: 1, scale: 1.1 }
-                      }
-                      exit={{ opacity: 0, y: -10 }}
-                      transition={{ duration: 0.25 }}
-                    >
-                      <IDForms></IDForms>
-                    </motion.div>
-                  </>
-                ) : (
-                  <>
-                    <motion.div
-                      initial={{ opacity: 0, y: 10 }}
-                      animate={
-                        cookies.currentUser
-                          ? { opacity: 1, y: 0 }
-                          : { opacity: 1, scale: 1.1 }
-                      }
-                      exit={{ opacity: 0, y: -10 }}
-                      transition={{ duration: 0.25 }}
-                    >
-                      <Info></Info>
-                    </motion.div>
-                  </>
-                )}
-              </motion.div>
-            </div>
+                <div>
+                  <h1>Scan to Verify LSCS Membership</h1>
+                </div>
+              </div>
+              {!('currentUser' in cookies) ? (
+                <>
+                  <motion.div
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={
+                      cookies.currentUser
+                        ? { opacity: 1, y: 0 }
+                        : { opacity: 1, scale: 1.1 }
+                    }
+                    exit={{ opacity: 0, y: -10 }}
+                    transition={{ duration: 0.25 }}
+                  >
+                    <IDForms></IDForms>
+                  </motion.div>
+                </>
+              ) : (
+                <>
+                  <motion.div
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={
+                      cookies.currentUser
+                        ? { opacity: 1, y: 0 }
+                        : { opacity: 1, scale: 1.1 }
+                    }
+                    exit={{ opacity: 0, y: -10 }}
+                    transition={{ duration: 0.25 }}
+                  >
+                    <Info></Info>
+                  </motion.div>
+                </>
+              )}
+            </motion.div>
           </div>
         </div>
       </AnimatePresence>
